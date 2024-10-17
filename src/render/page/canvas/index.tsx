25af68d437debcc5fa96dbde91721e67d0ce625f
djxc <dj_jxc@163.com>
2024-10-17 23:23:28 +0800

    canvas学习计算机图形学

--- src/render/page/canvas/index.tsx
+++ src/render/page/canvas/index.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+
 function CanvasPage() {
+    useEffect(() => {
+
+    }, [])
+
     return(
         <div>
            CanvasPage
+           <canvas id="djCanvas" width={20} height={20}></canvas>
         </div>
     )
 }
